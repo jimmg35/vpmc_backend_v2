dc7676e64c48cedb94f6755a666c0608664611ca
jimmg35 <jim60308@gmail.com>
2022-08-17 15:56:33 +0800

    完成google登入功能

--- src/controllers/AuthController/index.ts
+++ src/controllers/AuthController/index.ts
@@ -8,9 +8,23 @@ import { autoInjectable, inject } from "tsyringe"
 import sha256 from "fast-sha256"
 import StatusCodes from 'http-status-codes'
 import util from "tweetnacl-util"
+import https from 'https'
 import { JwtAuthenticator, tokenPayload } from "../../lib/JwtAuthenticator"
+import { generateVerificationToken } from "../../lib/util"
 
-const { OK, UNAUTHORIZED } = StatusCodes
+const { OK, UNAUTHORIZED, INTERNAL_SERVER_ERROR } = StatusCodes
+
+const validateGoogleToken = async (token: string): Promise<boolean> => {
+  return new Promise((resolve) => {
+    const url = `https://oauth2.googleapis.com/tokeninfo?id_token=${token}`
+    https.get(url, function (response) {
+      if (response.statusCode === 200) {
+        resolve(true)
+      }
+      resolve(false)
+    })
+  })
+}
 
 @autoInjectable()
 export default class AuthController extends BaseController {
@@ -20,6 +34,7 @@ export default class AuthController extends BaseController {
   public jwtAuthenticator: JwtAuthenticator
   public routeHttpMethod: { [methodName: string]: HTTPMETHOD; } = {
     "authenticate": "POST",
+    "googleAuth": "POST",
     "refresh": "POST",
     "validate": "POST",
     "listRoles": "GET"
@@ -105,6 +120,95 @@ export default class AuthController extends BaseController {
     return res.status(UNAUTHORIZED).json({
       "status": "帳號或密碼錯誤"
     })
+  }
+
+  /**
+   * @swagger
+   * /Auth/googleAuth:
+   *   post:
+   *     tags: 
+   *       - Auth
+   *     summary: 使用者Google Auth.
+   *     consumes:
+   *       - application/x-www-form-urlencoded
+   *     description: 於登入時使用
+   *     requestBody:
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *
+   *               email:
+   *                 required: true
+   *                 default: jim60308@gmail.com
+   *                 type: string
+   *                 description: 電子郵件
+   *             
+   *               token:
+   *                 required: true
+   *                 type: string
+   *                 description: google OAuth 回傳的token, 用於後端驗證
+   *     responses:
+   *       '200':    # status code
+   *         description: accessToken
+   *         content:
+   *           application/json:
+   *             schema: 
+   *               type: object
+   */
+  public googleAuth = async (req: Request, res: Response) => {
+    const params_set = { ...req.body }
+    const user_repository = this.dbcontext.connection.getRepository(User)
+    const user = await user_repository.findOne({ email: params_set.email as string })
+
+    if (!user) { // 如果使用者第一次用google auth登入系統
+
+      const status = await validateGoogleToken(params_set.token)
+      if (status) {
+        const user_repository = this.dbcontext.connection.getRepository(User)
+        const role_repository = this.dbcontext.connection.getRepository(Role)
+        const user = new User()
+        user.username = params_set.email.split('@')[0]
+        user.email = params_set.email
+        user.password = 'google auth'
+        user.roles = await role_repository.find({
+          code: 'user:basic'
+        })
+        user.mailConfirmationToken = generateVerificationToken(128)
+        await user_repository.save(user)
+        const registeredUser = await user_repository.findOne({ email: params_set.email as string })
+        if (!registeredUser) return res.status(INTERNAL_SERVER_ERROR).json({
+          "status": "user hasn't registered"
+        })
+        const token = this.jwtAuthenticator.signToken({
+          _userId: registeredUser.userId,
+          username: registeredUser.username,
+          email: registeredUser.email,
+          alias: registeredUser.alias
+        })
+        return res.status(OK).json({
+          "token": token
+        })
+      }
+      return res.status(UNAUTHORIZED).json({
+        "status": "google token auth failed"
+      })
+
+    } else { // 如果使用者已經用google auth登入過
+      user.lastLoginTime = new Date()
+      console.log(`user ${user.username} login at ${user.lastLoginTime}`)
+      await user_repository.save(user)
+      const token = this.jwtAuthenticator.signToken({
+        _userId: user.userId,
+        username: user.username,
+        email: user.email,
+        alias: user.alias
+      })
+      return res.status(OK).json({
+        "token": token
+      })
+    }
   }
 
   public refresh = async (req: Request, res: Response) => {
