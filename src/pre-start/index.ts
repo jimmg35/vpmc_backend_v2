218edeb294ec7c23ff43cd18cf3b8bef8d272857
jimmg35 <jim60308@gmail.com>
2022-10-03 10:42:31 +0800

    damn

--- src/pre-start/index.ts
+++ src/pre-start/index.ts
@@ -15,7 +15,7 @@ import commandLineArgs from 'command-line-args'
   ])
 
   const envConfig = dotenv.config({
-    path: path.join(__dirname, `../../envConfig/service-config/development.env`),
+    path: path.join(__dirname, `../../envConfig/service-config/production.env`),
   })
 
   if (envConfig.error) {
